94c148adf1d0f4c347b1857a4a1eda14a96020de
Duy <etn218n@gmail.com>
2023-01-20 16:03:39 +0200

    Test build command

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+
+using System.Linq;
+using UnityEditor;
+
+public static class Builder
+{
+    public static void Build() {
+        var options = new BuildPlayerOptions {
+            scenes = EditorBuildSettings.scenes.Select(scene => scene.path).ToArray(),
+            target = BuildTarget.StandaloneWindows64,
+            locationPathName = @"C:\Users\Duy\Projects\TempBuild\LearnDocker.exe"
+        };
+
+        BuildPipeline.BuildPlayer(options);
+    }
+}
+
+#endif
