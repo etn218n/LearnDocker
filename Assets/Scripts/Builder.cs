86a9527d8da0dc3372e4ceb021f3f9ba8d2f8adf
Duy <etn218n@gmail.com>
2023-01-20 16:33:28 +0200

    Fix output directory for container

--- Assets/Scripts/Builder.cs
+++ Assets/Scripts/Builder.cs
@@ -9,7 +9,7 @@ public static class Builder
         var options = new BuildPlayerOptions {
             scenes = EditorBuildSettings.scenes.Select(scene => scene.path).ToArray(),
             target = BuildTarget.StandaloneWindows64,
-            locationPathName = @"C:\Users\Duy\Projects\TempBuild\LearnDocker.exe"
+            locationPathName = @"C:\Users\Projects\TempBuild\LearnDocker.exe"
         };
 
         BuildPipeline.BuildPlayer(options);
